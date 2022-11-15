566065c068be0d4f138c0bd80a91bd89810d2c4a
Huong <htnguuyen@gmail.com>
2022-11-14 23:33:25 -0500

    Added router and Auth0

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,17 @@
 import React from 'react';
 import { createRoot } from "react-dom/client";
+import { Auth0Provider } from '@auth0/auth0-react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Auth0ProviderWithHistory from './auth0Provider.jsx';
 import App from "./components/App.jsx"
+
+
 const root = createRoot(document.getElementById("root"));
 
-root.render(<App />);
+root.render(
+  <Router>
+    <Auth0ProviderWithHistory>
+      <App />
+    </Auth0ProviderWithHistory>
+  </Router>
+  );
