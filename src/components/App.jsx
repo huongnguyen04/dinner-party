ac28a3fcd67203fc26b0fdc1db5dfd86f4a3a345
Huong <htnguuyen@gmail.com>
2022-11-14 23:31:24 -0500

    Added auth0 authentication with login

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,47 @@
 import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+import PartyOverview from './PartyOverview.jsx';
+import Menu from './Menu.jsx';
+import Guests from './Guests.jsx';
+
 
 const App = () => {
+  const { isLoading, isAuthenticated, error, user, loginWithRedirect, logout } =
+  useAuth0();
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div>Oops... {error.message}</div>;
+  }
 
   return (
-    <h1>dinner party 🥘🎉</h1>
+    <>
+      <h1>dinner party 🥘🎉</h1>
+      {!isAuthenticated &&
+      <>
+        <button onClick={loginWithRedirect}>Log in</button>
+        <h1>let's host a dinner party.</h1>
+        <h2>the theme is...</h2>
+      </>
+      }
+      {isAuthenticated &&
+        <>
+          <div>
+            Hello, {user.name}{'👋🏻 '}
+            <button onClick={() => logout({ returnTo: window.location.origin })}>
+              Log out
+            </button>
+          </div>
+          <div>
+            <PartyOverview/>
+            <Menu/>
+            <Guests/>
+          </div>
+        </>
+      }
+    </>
   )
 }
 
