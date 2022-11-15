c2e1de7716d2cd9fc2d51dafadcb7996fcd8fdfe
Huong <htnguuyen@gmail.com>
2022-11-14 23:35:24 -0500

    set up guests component

---
+++ src/components/Guests.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Guests = () => {
+
+  return(
+    <>
+      <h1>Guests</h1>
+
+      <div>Confirmed | Not Confirmed</div>
+
+      <button>invite more guests</button>
+    </>
+  );
+}
+
+export default Guests;
