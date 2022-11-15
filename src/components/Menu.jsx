a180203a4365823e2bc4af38d06af1ea57c537e0
Huong <htnguuyen@gmail.com>
2022-11-14 23:35:57 -0500

    set up basic component for menu

---
+++ src/components/Menu.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Menu = () => {
+
+  return(
+    <>
+      <h1>Menu</h1>
+
+      <h4>Entrees</h4>
+      <h4>Appetizers</h4>
+      <h4>Sides</h4>
+      <h4>Drinks</h4>
+      <h4>Dessert</h4>
+    </>
+  );
+}
+
+export default Menu;
