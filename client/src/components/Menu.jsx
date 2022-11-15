86d2bf95df35c0a65e7f50c70eb9130ee6f1423e
Huong <htnguuyen@gmail.com>
2022-11-15 00:38:17 -0500

    added states and buttons

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Menu = () => {
+  const [entrees, setEntrees] = useState(null);
+  const [appetizers, setAppetizers] = useState(null);
+  const [sides, setSides] = useState(null);
+  const [drinks, setDrinks] = useState(null);
+  const [desserts, setDesserts] = useState(null);
 
   return(
     <>
       <h1>Menu</h1>
 
       <h4>Entrees</h4>
+      <button>+</button>
+
       <h4>Appetizers</h4>
+      <button>+</button>
+
       <h4>Sides</h4>
+      <button>+</button>
+
       <h4>Drinks</h4>
+      <button>+</button>
+
       <h4>Dessert</h4>
+      <button>+</button>
     </>
   );
 }
