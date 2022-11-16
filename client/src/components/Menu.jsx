ec98a0ef3819123f2ef44ab0d6bdc1c601d1ccd5
Huong <htnguuyen@gmail.com>
2022-11-15 23:34:26 -0500

    set separate states for categories and added item

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -1,64 +1,97 @@
 import React, { useState } from 'react';
+import MenuEntry from './MenuEntry.jsx';
 
 const Menu = () => {
-  const [entrees, setEntrees] = useState(null);
-  const [appetizers, setAppetizers] = useState(null);
-  const [sides, setSides] = useState(null);
-  const [drinks, setDrinks] = useState(null);
-  const [desserts, setDesserts] = useState(null);
+  const [entrees, setEntrees] = useState(['entree1', 'entree2', 'entree3']);
+  const [appetizers, setAppetizers] = useState(['app1', 'app2', 'app3']);
+  const [sides, setSides] = useState(['side1', 'side2', 'side3']);
+  const [drinks, setDrinks] = useState(['drink1', 'drink2', 'drink3']);
+  const [desserts, setDesserts] = useState(['dessert1', 'dessert2', 'dessert3']);
+
+  const [entree, setEntree] = useState('');
+  const [appetizer, setAppetizer] = useState('');
+  const [side, setSide] = useState('');
+  const [drink, setDrink] = useState('');
+  const [dessert, setDessert] = useState('');
+
+  const addEntreeClick = () => {
+    let copy = entrees.slice();
+    copy.push(entree);
+    setEntrees(copy);
+    setEntree('');
+  }
+
+  const addAppClick = () => {
+    let copy = appetizers.slice();
+    copy.push(appetizer);
+    setAppetizers(copy);
+    setAppetizer('');
+  }
+
+  const addSideClick = () => {
+    let copy = sides.slice();
+    copy.push(side);
+    setSides(copy);
+    setSide('');
+  }
+
+  const addDrinkClick = () => {
+    let copy = drinks.slice();
+    copy.push(drink);
+    setDrinks(copy);
+    setDrink('');
+  }
+
+  const addDessertClick = () => {
+    let copy = desserts.slice();
+    copy.push(dessert);
+    setDesserts(copy);
+    setDessert('');
+  }
 
   return (
     <>
       <h1>Menu</h1>
 
       <h4>Entrees</h4>
-      <div>entree 1</div>
-      <div>entree 2</div>
-      <div>entree 3</div>
+
+      <MenuEntry foods={entrees}/>
       <br></br>
       <div>
-        <input placeholder={"add entree"} value={entrees} onChange={(e) => setEntrees(e.target.value)}></input>
-        <button>+</button>
+        <input placeholder={"add entree"} value={entree} onChange={(e) => setEntree(e.target.value)}></input>
+        <button onClick={addEntreeClick}>+</button>
       </div>
 
       <h4>Appetizers</h4>
-      <div>app 1</div>
-      <div>app 2</div>
-      <div>app 3</div>
+      <MenuEntry foods={appetizers}/>
       <br></br>
       <div>
-        <input placeholder={"add app"} value={appetizers} onChange={(e) => setAppetizers(e.target.value)}></input>
-        <button>+</button>
+        <input placeholder={"add app"} value={appetizer} onChange={(e) => setAppetizer(e.target.value)}></input>
+        <button onClick={addAppClick}>+</button>
       </div>
 
       <h4>Sides</h4>
-      <div>side 1</div>
-      <div>side 2</div>
-      <div>side 3</div>
+      <MenuEntry foods={sides}/>
       <br></br>
       <div>
-        <input placeholder={"add side"} value={sides} onChange={(e) => setSides(e.target.value)}></input>
-        <button>+</button>
+        <input placeholder={"add side"} value={side} onChange={(e) => setSide(e.target.value)}></input>
+        <button onClick={addSideClick}>+</button>
       </div>
 
       <h4>Drinks</h4>
-      <div>drink 1</div>
-      <div>drink 2</div>
-      <div>drink 3</div>
+      <MenuEntry foods={drinks}/>
       <br></br>
       <div>
-        <input placeholder={"add drink"} value={drinks} onChange={(e) => setDrinks(e.target.value)}></input>
-        <button>+</button>
+        <input placeholder={"add drink"} value={drink} onChange={(e) => setDrink(e.target.value)}></input>
+        <button onClick={addDrinkClick}>+</button>
       </div>
 
       <h4>Dessert</h4>
-      <div>dessert 1</div>
-      <div>dessert 2</div>
-      <div>dessert 3</div>
+      <MenuEntry foods={drinks}/>
       <br></br>
       <div>
-        <input placeholder={"add dessert"} value={desserts} onChange={(e) => setDesserts(e.target.value)}></input>
-        <button>+</button>
+        <input placeholder={"add dessert"} value={dessert} onChange={(e) => setDessert(e.target.value)}></input>
+        <button onClick={addDessertClick}>+</button>
       </div>
     </>
   );
