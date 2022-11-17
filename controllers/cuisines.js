3eef101e89c7c2e460d2f4c0a0569931a6fb8e53
Huong <htnguuyen@gmail.com>
2022-11-16 22:10:15 -0500

    added route handlers

--- controllers/cuisines.js
+++ controllers/cuisines.js
@@ -1,7 +1,8 @@
-const Cuisine = require('../models/cuisine.js');
+// const Cuisine = require('../models/cuisine.js');
+const { Cuisine } = require('../db/connection.js');
+
 
 const getCuisines = (req, res) => {
-  console.log('in /cuisines route')
   Cuisine.distinct('type')
     .then((cuisines) => {
       console.log('cuisines: ', cuisines)
@@ -11,21 +12,31 @@ const getCuisines = (req, res) => {
 }
 
 const getCuisinesData = (req, res) => {
-  console.log('req.params: ', req.params.theme)
   let theme = req.params.theme;
-  console.log('in /cuisines route')
   let data = {};
-  Cuisine.find({'type': theme, 'category': 'entree'}, null, { limit: 3 })
-    .then((cuisines) => {
-      console.log('cuisines: ', cuisines)
-      data.entrees = cuisines;
-      Cuisine.find({'type': theme, 'category': 'appetizers'}, null, { limit: 3 })
-        .then((result) => {
-          data.appetizers = result;
-          console.log('data: ', data)
-          res.send(data);
-        })
+  Cuisine.find({'type': theme})
+    .then((result) => {
+      console.log('cuisines: ', result)
+      data.entrees = [];
+      data.appetizers = [];
+      data.sides = [];
+      data.drinks = [];
+      data.desserts = [];
 
+      result.forEach((item) => {
+        if (item.category === 'entree') {
+          data.entrees.push(item);
+        } else if (item.category === 'appetizer') {
+          data.appetizers.push(item);
+        } else if (item.category === 'side') {
+          data.sides.push(item);
+        } else if (item.category === 'drink') {
+          data.drinks.push(item);
+        } else if (item.category === 'dessert') {
+          data.desserts.push(item);
+        }
+      })
+      res.send(data);
     })
     .catch(console.error)
 }
