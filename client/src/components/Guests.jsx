66c871adc60ab7ec92d77e4643d4fceb9a245fce
Huong <htnguuyen@gmail.com>
2022-11-16 22:05:39 -0500

    added toggle functionality for confirmed and non-confirmed guests

--- client/src/components/Guests.jsx
+++ client/src/components/Guests.jsx
@@ -1,14 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
+import GuestEntry from './GuestEntry.jsx';
 
-const Guests = () => {
+const Guests = ({ guests, setGuests, watch, setWatch, addGuest }) => {
+  const [guest, setGuest] = useState('');
+  const [confirmedView, setConfirmedView] = useState(true);
 
-  return(
+
+  const onGuestButtonClick = () => {
+    setWatch(!watch);
+    addGuest(guest);
+    setGuest('');
+  }
+
+  let confirmedList;
+  let nonConfirmedList;
+
+  if (guests && guests.length > 0) {
+    confirmedList = guests.filter(() => guests.confirmed);
+    nonConfirmedList = guests.filter(() => !guests.confirmed);
+  }
+
+  const onConfirmedClick = () => {
+    setConfirmedView(true);
+  }
+
+  const onNotConfirmedClick = () => {
+    setConfirmedView(false);
+  }
+
+  return (
     <>
       <h1>Guests</h1>
 
-      <div>Confirmed | Not Confirmed</div>
+      <button onClick={onConfirmedClick}>Confirmed</button> <button onClick={onNotConfirmedClick}>Not Confirmed</button>
 
-      <button>invite more guests</button>
+      {confirmedView && <GuestEntry guests={confirmedList}/>}
+      {!confirmedView && <GuestEntry guests={nonConfirmedList}/>}
+
+      <div>
+        <input placeholder={"add more guests"} value={guest} onChange={(e) => setGuest(e.target.value)}></input>
+        <button onClick={onGuestButtonClick}>+</button>
+      </div>
+
     </>
   );
 }
