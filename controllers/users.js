b373aaf7ad9c43af682baeb563c834050d1c9d6c
Huong <htnguuyen@gmail.com>
2022-11-17 03:36:47 -0500

    added party detail handler

--- controllers/users.js
+++ controllers/users.js
@@ -9,17 +9,29 @@ const getPartyData = (req, res) => {
       console.log('party data: ', data)
       res.send(data);
     })
-    .catch(console.error)
+    .catch((err) => console.log('Error, could not get all party data in database. Error: ', err))
 }
 
 const addPartyDetail = (req, res) => {
+  User.findOneAndUpdate({userId: req.body.userId}, {theme: req.body.theme, date: req.body.date, host: req.body.host}, {returnDocument: 'after', upsert: true})
+    .then((data) => {
+      res.send('Successfully added guest to database');
+    })
+    .catch((err) => console.log('Error, could not add guest to database. Error: ', err));
+}
 
+const clearMenu = (req, res) => {
+  User.deleteMany({})
+    .then(() => {
+      res.send('Deleted all menu items from the database!');
+    })
+    .catch((err) => console.log('Error, could not delete all menu items from database. Error: ', err))
 }
 
 const addGuest = (req, res) => {
   // console.log('req.body: ', req.body)
   console.log('in addGuest handler')
-  User.findOneAndUpdate({userId: req.body.userId}, {'$push': {guests: {name: req.body.guest, confirmed: true }}}, {returnDocument: 'after', upsert: true})
+  User.findOneAndUpdate({userId: req.body.userId}, {'$push': {guests: {name: req.body.guest, confirmed: req.body.confirmed }}}, {returnDocument: 'after', upsert: true})
     .then((data) => {
       res.send('Successfully added guest to database');
     })
@@ -71,4 +83,4 @@ const addDessert = (req, res) => {
     .catch((err) => console.log('Error, could not add dessert to database. Error: ', err));
 }
 
-module.exports = { getPartyData, addGuest, addPartyDetail, addEntree, addAppetizer, addSide, addDrink, addDessert }
+module.exports = { getPartyData, addGuest, addPartyDetail, clearMenu, addEntree, addAppetizer, addSide, addDrink, addDessert }
