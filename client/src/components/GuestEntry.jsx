f3fe078f4dfec18f62c0c671719e5282c192fd9d
Huong <htnguuyen@gmail.com>
2022-11-17 03:35:14 -0500

    added confirmed and non-confirmed indicators

--- client/src/components/GuestEntry.jsx
+++ client/src/components/GuestEntry.jsx
@@ -1,11 +1,29 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 
-const GuestEntry = ({ guests }) => {
+const GuestEntry = ({ guests, setGuests }) => {
+
+  const removeConfirmation = (guest) => {
+    let copyGuests = guests.slice();
+    let index = copyGuests.indexOf(guest);
+    guest.confirmed = false;
+    copyGuests[index] = guest;
+    setGuests(copyGuests);
+  }
+
+  const addConfirmation = (guest) => {
+    let copyGuests = guests.slice();
+    let index = copyGuests.indexOf(guest);
+    guest.confirmed = true;
+    copyGuests[index] = guest;
+    setGuests(copyGuests);
+  }
+
 
   let guestList;
   if (guests && guests.length > 0) {
     guestList = guests.map((guest, index) => {
-      return <div key={index}>{guest.name}</div>
+      return <StyledGuest key={index}>{guest.confirmed ? <span onClick={() => removeConfirmation(guest)}>✓</span> : <span onClick={() => addConfirmation(guest)}>☐</span>} {guest.name}</StyledGuest>
     })
   }
   return (
@@ -14,5 +32,13 @@ const GuestEntry = ({ guests }) => {
     </>
   )
 }
+
+const StyledGuest = styled.div`
+  padding: 2px;
+  margin: 2px;
+  span {
+    cursor: pointer;
+  }
+`
 
 export default GuestEntry;
--- client/src/components/Guests.jsx
+++ client/src/components/Guests.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 import GuestEntry from './GuestEntry.jsx';
 
 const Guests = ({ guests, setGuests, watch, setWatch, addGuest }) => {
   const [guest, setGuest] = useState('');
-  const [confirmedView, setConfirmedView] = useState(true);
+  const [showAllView, setShowAllView] = useState(true);
+  const [confirmedView, setConfirmedView] = useState(false);
+  const [nonConfirmedView, setNonConfirmedView] = useState(false)
 
 
   const onGuestButtonClick = () => {
@@ -16,26 +19,40 @@ const Guests = ({ guests, setGuests, watch, setWatch, addGuest }) => {
   let nonConfirmedList;
 
   if (guests && guests.length > 0) {
-    confirmedList = guests.filter(() => guests.confirmed);
-    nonConfirmedList = guests.filter(() => !guests.confirmed);
+    confirmedList = guests.filter((guest) => guest.confirmed);
+    nonConfirmedList = guests.filter((guest) => !guest.confirmed);
+  }
+
+  const onAllClick = () => {
+    setShowAllView(true);
+    setConfirmedView(false);
+    setNonConfirmedView(false);
   }
 
   const onConfirmedClick = () => {
     setConfirmedView(true);
+    setShowAllView(false);
+    setNonConfirmedView(false);
   }
 
   const onNotConfirmedClick = () => {
+    setNonConfirmedView(true);
     setConfirmedView(false);
+    setShowAllView(false);
   }
+
 
   return (
     <>
       <h1>Guests</h1>
-
-      <button onClick={onConfirmedClick}>Confirmed</button> <button onClick={onNotConfirmedClick}>Not Confirmed</button>
-
-      {confirmedView && <GuestEntry guests={confirmedList}/>}
-      {!confirmedView && <GuestEntry guests={nonConfirmedList}/>}
+      <StyledBtn selected={showAllView} onClick={onAllClick}>All Guests</StyledBtn>
+      <StyledBtn selected={confirmedView} onClick={onConfirmedClick}>Confirmed</StyledBtn>
+      <StyledBtn selected={nonConfirmedView} onClick={onNotConfirmedClick}>Not Confirmed</StyledBtn>
+      <StyledGuestsArea>
+        {showAllView && <GuestEntry guests={guests} setGuests={setGuests}/>}
+        {confirmedView && <GuestEntry guests={confirmedList} setGuests={setGuests}/>}
+        {nonConfirmedView && <GuestEntry guests={nonConfirmedList} setGuests={setGuests}/>}
+      </StyledGuestsArea>
 
       <div>
         <input placeholder={"add more guests"} value={guest} onChange={(e) => setGuest(e.target.value)}></input>
@@ -45,5 +62,14 @@ const Guests = ({ guests, setGuests, watch, setWatch, addGuest }) => {
     </>
   );
 }
+
+const StyledGuestsArea = styled.div`
+  min-height: 300px;
+  border: 1px solid black;
+`
+
+const StyledBtn = styled.button`
+  background: ${props => props.selected ? '#D3D3D3' : 'none'}
+`
 
 export default Guests;
