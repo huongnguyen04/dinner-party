c5645699ed707415d8a1c0c0165116e2618bef00
Huong <htnguuyen@gmail.com>
2022-11-16 22:12:17 -0500

    created userSchema

--- models/user.js
+++ models/user.js
@@ -1,9 +1,23 @@
-const mongoose = require('mongoose')
-const UserSchema = new mongoose.Schema ({
-  name: {
-    type: String,
-  }
-})
-const User = mongoose.model('User', UserSchema);
+const mongoose = require('mongoose');
 
-module.exports = User;
+const UserSchema = new mongoose.Schema({
+  userId: String,
+  theme: String,
+  date: String,
+  host: String,
+  entrees: [String],
+  appetizers: [String],
+  sides: [String],
+  drinks: [String],
+  desserts: [String],
+  guests: [
+    {
+      name: String,
+      confirmed: { type: Boolean, default: false }
+    }
+  ]
+});
+
+// const User = mongoose.model('User', UserSchema);
+
+module.exports = UserSchema;
