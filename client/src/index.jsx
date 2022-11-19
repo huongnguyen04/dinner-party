b0faf11a70b6d4c64cddc0d4ac9537117cfdd4bc
Huong <htnguuyen@gmail.com>
2022-11-19 01:38:52 -0500

    renamed Auth0ProviderWithHistory component to Auth0ProviderWithConfig

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,16 +2,15 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Auth0Provider } from '@auth0/auth0-react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import Auth0ProviderWithHistory from './auth0Provider.jsx';
+import Auth0ProviderWithConfig from './auth0Provider.jsx';
 import App from './components/App.jsx'
-
 
 const root = createRoot(document.getElementById('root'));
 
 root.render(
   <Router>
-    <Auth0ProviderWithHistory>
+    <Auth0ProviderWithConfig>
       <App />
-    </Auth0ProviderWithHistory>
+    </Auth0ProviderWithConfig>
   </Router>
   );
