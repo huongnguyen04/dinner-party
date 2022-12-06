6688814ff16d033dcf7ec9e6189ba1df936f326f
Huong <htnguuyen@gmail.com>
2022-12-06 14:38:00 -0500

    removed spacingwq

--- client/src/components/Toggle.jsx
+++ client/src/components/Toggle.jsx
@@ -4,9 +4,7 @@ import styled from "styled-components"
 const Toggle = ({theme,  toggleTheme }) => {
   return (
     <StyledButtonContainer>
-      <Button onClick={toggleTheme} >
-        ☾
-      </Button>
+      <Button onClick={toggleTheme}>☾</Button>
     </StyledButtonContainer>
     );
   };
