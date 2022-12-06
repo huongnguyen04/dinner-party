d00b66f6ca9c33b03295e81daa5061d34e08ec1f
Huong <htnguuyen@gmail.com>
2022-12-05 23:34:59 -0500

    fixed indentation

--- client/src/components/AddGuestContent.jsx
+++ client/src/components/AddGuestContent.jsx
@@ -15,17 +15,15 @@ const AddGuestContent = ({ toggle, addGuest }) => {
 
   return (
     <>
-
-    <input placeholder={'guest name'} value={guestName} onChange={(e) => {
-      if (e.target.value.length > 0) {
-        setGuestName(e.target.value)
-      }}} required>
-    </input>
-    <br></br>
-    <input type='email' placeholder={'guest email'} value={guestEmail} onChange={(e) => setGuestEmail(e.target.value)}></input>
-    <br></br>
-    <button className='plus' onClick={onGuestButtonClick}>Add</button>
-
+      <input placeholder={'guest name'} value={guestName} onChange={(e) => {
+        if (e.target.value.length > 0) {
+          setGuestName(e.target.value)
+        }}} required>
+      </input>
+      <br></br>
+      <input type='email' placeholder={'guest email'} value={guestEmail} onChange={(e) => setGuestEmail(e.target.value)}></input>
+      <br></br>
+      <button className='plus' onClick={onGuestButtonClick}>Add</button>
     </>
   )
 }
