b0c0dc2c96304a95a0d5f6c16ba5670740c6c94a
Huong <htnguuyen@gmail.com>
2022-12-07 00:24:21 -0500

    added transform-origin

--- client/src/components/Toggle.jsx
+++ client/src/components/Toggle.jsx
@@ -1,11 +1,9 @@
-import React from 'react'
-import styled from "styled-components"
+import React from 'react';
+import styled from 'styled-components';
 
 const Toggle = ({theme,  toggleTheme }) => {
   return (
-    <StyledButtonContainer>
       <Button onClick={toggleTheme}>☾</Button>
-    </StyledButtonContainer>
     );
   };
 
@@ -18,11 +16,9 @@ const Button = styled.button`
   font-size:1.5rem;
   padding: 0.6rem;
   transform: rotate(-30deg);
+  transform-origin: bottom left;
+  user-select: none;
   }
 `
 
-const StyledButtonContainer = styled.div`
-  display: flex;
-  justify-content: right;
-`
 export default Toggle;
