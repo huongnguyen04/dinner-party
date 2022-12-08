671d8d4828fd25623510b4375b68e1f8898d605f
Huong <htnguuyen@gmail.com>
2022-12-07 22:28:14 -0500

    added conditional

--- controllers/users/invitation.js
+++ controllers/users/invitation.js
@@ -4,17 +4,21 @@ const getInvitations = (req, res) => {
   let parties = [];
   User.find({'partiesHosting.guests.email': req.params.userEmail})
     .then((data) => {
-      data[0].partiesHosting.forEach((party) => {
-        party.guests.forEach((guest) => {
-          if (guest.email === req.params.userEmail) {
-            parties.push([party, data[0].email]);
-          }
+      if (data[0].partiesHosting) {
+        data[0].partiesHosting.forEach((party) => {
+          party.guests.forEach((guest) => {
+            if (guest.email === req.params.userEmail) {
+              parties.push([party, data[0].email]);
+            }
+          })
         })
-      })
-      parties.sort(function(a,b) {
-        return new Date(a[0].date) - new Date(b[0].date);
-      })
-      res.send(parties);
+        parties.sort(function(a,b) {
+          return new Date(a[0].date) - new Date(b[0].date);
+        })
+        res.send(parties);
+      } else {
+        res.send('No Party Invites')
+      }
     })
     .catch((err) => console.log('Error, could not get invitations from database. Error: ', err))
 }
