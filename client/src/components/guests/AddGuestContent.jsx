8cc7b8dcfcce8b97e5357705bd2a7f67c40a4d04
Huong <htnguuyen@gmail.com>
2022-12-07 22:28:34 -0500

    fixed styling

--- client/src/components/guests/AddGuestContent.jsx
+++ client/src/components/guests/AddGuestContent.jsx
@@ -15,17 +15,40 @@ const AddGuestContent = ({ toggle, addGuest }) => {
 
   return (
     <>
-      <input placeholder={'guest name'} value={guestName} onChange={(e) => {
-        if (e.target.value.length > 0) {
-          setGuestName(e.target.value)
-        }}} required>
-      </input>
+    <StyledFlex>
+      <div>
+        <label for={guestName}>Guest Name: </label>
+        <NameInput placeholder={'guest name*'} value={guestName} onChange={(e) => {
+          if (e.target.value.length > 0) {
+            setGuestName(e.target.value)
+          }}} required>
+        </NameInput>
+        <br></br>
+        <label for={guestEmail}>Guest Email: </label>
+        <EmailInput type='email' placeholder={'guest email'} value={guestEmail} onChange={(e) => setGuestEmail(e.target.value)}></EmailInput>
+      </div>
       <br></br>
-      <input type='email' placeholder={'guest email'} value={guestEmail} onChange={(e) => setGuestEmail(e.target.value)}></input>
-      <br></br>
-      <button className='plus' onClick={onGuestButtonClick}>Add</button>
+      <button onClick={onGuestButtonClick}>Add</button>
+    </StyledFlex>
     </>
   )
 }
+
+const NameInput = styled.input`
+  display: inline block;
+  width: 100%;
+`
+
+const EmailInput = styled.input`
+  display: inline block;
+  width: 100%;
+`
+
+const StyledFlex = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
 
 export default AddGuestContent;
