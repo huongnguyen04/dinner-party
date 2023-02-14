36c5f5bd0e53bd265b27de0b4f395b4696a780ab
Huong <htnguuyen@gmail.com>
2023-02-14 02:25:47 -0500

    moved form to separate component

--- client/src/components/partyOverview/Form.jsx
+++ client/src/components/partyOverview/Form.jsx
@@ -1,7 +1,42 @@
 import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import ThemeInput from './ThemeInput.jsx';
+import ChooseTheme from './ChooseTheme.jsx';
+import RandomizedTheme from './RandomizedTheme.jsx';
+import { formatInput } from '../formatInput.js';
 
-const Form = () => {
+const Form = ({ cuisines, generateMenu, selectedTheme, setSelectedTheme, setTheme, toggle, themeOption }) => {
+  const [tempTheme, setTempTheme] = useState('');
+
   return (
-
+    <form onSubmit={(e)=> {
+      e.preventDefault();
+      setTheme(formatInput(tempTheme));
+      generateMenu(selectedTheme);
+      toggle();
+    }}>
+      <StyledFlex>
+        <StyledUserInput>
+          {themeOption === 'input' &&
+            <ThemeInput tempTheme={tempTheme} setTempTheme={setTempTheme} />}
+          {themeOption === 'list' && <ChooseTheme cuisines={cuisines} setTempTheme={setTempTheme}/>}
+          {themeOption === 'random' && <RandomizedTheme cuisines={cuisines} setTempTheme={setTempTheme}/>}
+        </StyledUserInput>
+      </StyledFlex>
+      <br></br>
+      <input type='submit'></input>
+    </form>
   )
 }
+
+const StyledFlex = styled.div`
+  margin-top: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+const StyledUserInput = styled.div`
+  text-align: right;
+`
+
+export default Form;
