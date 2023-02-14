0e4fc3c1e79cbdf87289c82f4b0ce699a0845218
Huong <htnguuyen@gmail.com>
2023-02-14 13:25:41 -0500

    file clean up

--- client/src/components/partyOverview/Form.jsx
+++ client/src/components/partyOverview/Form.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import ThemeInput from './ThemeInput.jsx';
 import ChooseTheme from './ChooseTheme.jsx';
