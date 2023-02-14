a515d7d6b3bbcb364092b6a1d9e7a9c5679e81a2
Huong <htnguuyen@gmail.com>
2023-02-14 02:27:16 -0500

    changed first view of form to only date and host

--- client/src/components/partyOverview/PartyDetailModalContent.jsx
+++ client/src/components/partyOverview/PartyDetailModalContent.jsx
@@ -1,35 +1,18 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import axios from 'axios';
 import { formatInput } from '../formatInput.js';
+import Form from './Form.jsx';
+import SelectThemeOption from './SelectThemeOption.jsx';
 
 
 const PartyDetailModalContent = ({ toggle, setTheme, setDate, setHost, generateMenu }) => {
-  const [modalView1, setModalView1] = useState(true);
-  const [userInput, setUserInput] = useState(null);
+  const [view, setView] = useState(true);
+  // const [userInput, setUserInput] = useState(null);
   const [cuisines, setCuisines] = useState(null);
-  const [tempTheme, setTempTheme] = useState('');
+  // const [tempTheme, setTempTheme] = useState('');
   const [tempDate, setTempDate] = useState('');
   const [tempHost, setTempHost] = useState('');
   const [selectedTheme, setSelectedTheme] = useState(null);
-
-  const getCuisines = () => {
-    axios.get(`/cuisines`)
-      .then((res) => {
-        console.log('response: ', res.data);
-        setCuisines(res.data);
-      })
-      .catch((err) => {
-        console.log('Error, could not get cuisines. Error: ', err);
-      })
-  }
-
-  let cuisineOptions;
-  if (cuisines) {
-    cuisineOptions = cuisines.map((cuisine, index) =>
-      <option key={index}>{cuisine}</option>
-    )
-  }
 
   const formatDate = (date) => {
     let formatted = '';
@@ -40,92 +23,35 @@ const PartyDetailModalContent = ({ toggle, setTheme, setDate, setHost, generateM
 
   return (
     <>
-    {modalView1 &&
+     {view &&
       <>
-        <StyledModalView>
-        <StyledQuestion>
-          Would you like to input a theme or choose from a list?
-        </StyledQuestion>
-        <br></br>
-          <select onChange={(e) => {
-              if (e.target.value === 'true') {
-                setUserInput(true);
-              } else {
-                setUserInput(false);
-                getCuisines();
-              }
-            }}>
-            <option value='select'>select an option</option>
-            <option value={true}>I want to input a theme</option>
-            <option value={false}>I want choose from a list</option>
-          </select>
-          <div>
-            <br></br>
-            <button className='nextButton' onClick={() => setModalView1(!modalView1)}>Next</button>
-          </div>
-        </StyledModalView>
-      </>
-    }
-     {!modalView1 && !userInput &&
-      <>
-        <StyledModalView>
-          <div>Select an option and we'll generate a menu for you.</div>
-          <br></br>
-          <form onSubmit={(e)=> {
-            e.preventDefault();
-            setTheme(formatInput(tempTheme + ' cuisine'));
-            setDate(formatDate(tempDate));
-            setHost(formatInput(tempHost));
-            generateMenu(selectedTheme);
-            toggle();
-          }}>
-            <StyledFlex>
-              <StyledUserInput>
-                <label htmlFor='theme'>Theme: </label>
-                <select className='selectTheme' onChange={(e) => {
-                  setSelectedTheme(e.target.value);
-                  setTempTheme(e.target.value);
-                }}>
-                  <option>select a theme</option>
-                  {cuisineOptions}
-                </select>
-                <div>
-                  Date: <input type='date' value={tempDate} onChange={(e) => setTempDate(e.target.value)} required></input>
-                </div>
-                <div>
-                  Host: <input type='text' value={tempHost} onChange={(e) => setTempHost(e.target.value)} required></input>
-                </div>
-              </StyledUserInput>
-            </StyledFlex>
-            <br></br>
-            <input type='submit'></input>
-          </form>
-        </StyledModalView>
-       </>
-    }
-    {!modalView1 && userInput &&
       <StyledModalView>
+        <div>First, enter the party's host and date.</div>
+        <br></br>
         <form onSubmit={(e)=> {
           e.preventDefault();
-          setTheme(formatInput(tempTheme));
           setDate(formatDate(tempDate));
           setHost(formatInput(tempHost));
-          toggle();
+          setView(!view);
         }}>
-          <StyledFlex>
-            <StyledUserInput>
-              Theme: <input type='text' value={tempTheme} onChange={(e) => setTempTheme(e.target.value)} required></input>
-              <br></br>
-              Date: <input type='date' value={tempDate} onChange={(e) => setTempDate(e.target.value)} required></input>
-              <br></br>
-              Host: <input type='text' value={tempHost} onChange={(e) => setTempHost(e.target.value)} required></input>
-            </StyledUserInput>
-          </StyledFlex>
-          <br></br>
-          <br></br>
-          <input type='submit'></input>
-        </form>
-      </StyledModalView>
+      <StyledFlex>
+        <StyledUserInput>
+          <div>
+            Date: <input type='date' value={tempDate} onChange={(e) => setTempDate(e.target.value)} required></input>
+          </div>
+          <div>
+            Host: <input type='text' value={tempHost} onChange={(e) => setTempHost(e.target.value)} required></input>
+          </div>
+        </StyledUserInput>
+      </StyledFlex>
+      <br></br>
+      <input type='submit' value={"next"}></input>
+    </form>
+    </StyledModalView>
+    </>
+     }
+    {!view &&
+      <SelectThemeOption cuisines={cuisines} setCuisines={setCuisines} generateMenu={generateMenu} selectedTheme={selectedTheme} setSelectedTheme={setSelectedTheme} setTheme={setTheme} toggle={toggle}/>
     }
   </>
   )
