0a6a246b385ca32dd76dcd5a8386f3fda9f2a1bf
Huong <htnguuyen@gmail.com>
2023-02-14 13:25:19 -0500

    file clean up

--- client/src/components/partyOverview/ThemeInput.jsx
+++ client/src/components/partyOverview/ThemeInput.jsx
@@ -1,14 +1,14 @@
-import React, { useState } from 'react'
+import React from 'react';
 
 const ThemeInput = ({ tempTheme, setTempTheme }) => {
 
   return (
     <>
+      <div>Enter your theme below.</div>
+      <br></br>
       <span>Theme: </span>
-      <input type='text' value={tempTheme} onChange={(e) => {
-        setTempTheme(e.target.value);
-      }
-        } required></input>
+      <input type='text' value={tempTheme} onChange={(e) =>
+        setTempTheme(e.target.value)} required></input>
     </>
   )
 };
